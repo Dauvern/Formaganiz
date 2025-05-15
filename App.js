979063397b3e1284d69e8140e99a159d39d55c15
Dauvern <nicolas.dauvert@educagri.fr>
2025-05-15 10:05:57 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial" }}>
+      <h1>Bienvenue dans FamOrganiz</h1>
+      <p>Votre application familiale pour gérer les repas, les plannings et les tâches.</p>
+    </div>
+  );
+}
+
+export default App;
